f4120f9aef07347deecb0f8e88fe14203f79a7c4
Kaitlin Rosen <kayejrosen@gmail.com>
2022-05-26 19:01:52 -0700

    Initial commit

---
+++ model.py
@@ -0,0 +1,8 @@
+"""ASCII drawing API
+
+render_canvas with specific height and width
+add_shape to a canvas
+clear_all shapes from a canvas
+create_rectangle
+change_fill character for a rectangle
+traverse up, down, left, right"""
